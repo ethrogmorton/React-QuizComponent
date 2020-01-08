d7deea6434dc2a6b4aec2d77a9f9e510ce6083c1
ethrogmorton <57643915+ethrogmorton@users.noreply.github.com>
2020-01-08 09:35:53 -0500

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
+     constructor(props) {
+          super(props)
+          
+     }
 	render(){
           return (
                <div>Quiz</div>
-          )
-     }
+          
+          )}
+     
 }
 export default Quiz;
